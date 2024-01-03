480411ed521d560284578506a45276edc8cb23eb
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-03 21:34:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,11 @@ DIR = f"{os.getcwd()}/"
 
 #init section
 
+video_formats = ['mp4', 'avi', 'mkv', 'mov', 'wmv', 'flv', 'webm', '3gp']
+audio_formats = ['mp3', 'wav', 'ogg', 'aac', 'flac', 'wma', 'm4a']
+text_formats = ['txt', 'csv', 'json', 'xml', 'html', 'md', 'pdf']
+coding_languages = ['py', 'java', 'js', 'c', 'css', 'ruby', 'php', 'swift']
+
 a_id = "10187126" # Your Api Id
 a_hash = "ff197c0d23d7fe54c89b44ed092c1752" # Your Api Hash 
 b_tok = "6196780979:AAEn9mTjzwGLeevJBC2h5dhpixKkuHtMEfU" # Your Bot Token
@@ -25,19 +30,21 @@ def rename(_, message):
 
     try:
         filename = message.text.replace(message.text.split(" ")[0], "")
-            
+        if not filename.endswith(video_formats, audio_formats, text_formats, coding_languages):
+            message.reply_text("Please Enter Text Or Please Enter Correct Formats Like .mp4")
+        else:
+            if reply := message.reply_to_message:
+                x = message.reply_text("Downloading.....")
+                path = reply.download(file_name=filename)
+                x.edit("Uploading.....")
+                message.reply_document(path, thumb=thumb_id, caption=filename)
+                os.remove(path)
+            else:
+                bot.send_message(message.chat.id, "**USAGE** `/rename` [file name] And Reply A media")
     except Exception as e:
         print(e)
-
-    if reply := message.reply_to_message:
-        x = message.reply_text("Downloading.....")
-        path = reply.download(file_name=filename)
-        x.edit("Uploading.....")
-        message.reply_document(path, thumb=thumb_id, caption=filename)
-        os.remove(path)
-    else:
-        bot.send_message(message.chat.id, "**USAGE** `/rename` [file name] And Reply A media")
-
+        message.reply_text("**Error**", e)
+        
 @bot.on_message(filters.command("start"))
 def start(_, message):
     bot.send_message(message.chat.id, "**USAGE** `/rename` [file name] And Reply A media")
