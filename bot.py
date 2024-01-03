1c468b0776d9747355eae424a2ec58d11d0c8536
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-03 20:51:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,32 @@ def rename(_, message):
 
     try:
         filename = message.text.replace(message.text.split(" ")[0], "")
-
+        if filename.endswith(".mp4"):
+            print(None)
+        elif filename.endswith(".mkv"):
+            print(None)
+        elif filename.endswith(".mp3"):
+            print(None)
+        elif filename.endswith(".jpeg"):
+            print(None)
+        elif filename.endswith(".png"):
+            print(None)
+        elif filename.endswith(".jpg"):
+            print(None)
+        elif filename.endswith(".py"):
+            print(None)
+        elif filename.endswith(".java"):
+            print(None)
+        elif filename.endswith(".txt"):
+            print(None)
+        elif filename.endswith(".text"):
+            print(None)
+        elif filename.endswith(".ogg"):
+            print(None)
+        elif filename.endswith(".m4a"):
+            print(None)
+        elif filename.endswith("crt"):
+            print(None)
     except Exception as e:
         print(e)
 
